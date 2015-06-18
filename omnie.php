c143f1eafa7174e1415c1f1dd70f5faf8c45aedf
Agnieszka Górz <agnieszka.gorz@outlook.com>
2015-06-18 19:06:57 +0200

    last commit 18.06

--- omnie.php
+++ omnie.php
@@ -1,54 +1,37 @@
-<!doctype html>
-<html lang="pl-PL">
-<head>
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui ">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-	<title>Izabela Górz - moja historia</title>
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="font-awesome-4.3.0/css/font-awesome.min.css">
-    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-	
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <link href="css/style.css" rel="stylesheet">
+<?php
+    include 'inc/top.inc.php';
+?>
+    <title>Izabela Górz - moja historia</title>
     <link href="css/omnie.css" rel="stylesheet">
     
 
 </head>
 <body>
-	<?php
-		include 'inc/menu.inc.php';
-	?>
-	<div class="container">
+<div class="container">
+		<?php
+            include 'inc/menu.inc.php';
+        ?>
 		<div id="top" class="col-sm-12">
-        	<h1><span>O mnie</span></h1>
+            <h1><span>Moja przeciętnie nieprzeciętna historia</span></h1>
         </div>
-        <div class="container">
-        <div id="left" class="pull-left col-sm-6">
-        	<h2>Zdjęcie profilowe</h2>
+        	
+        <div class="col-sm-6 hexagon">
+            <div class="hexTop"></div>
+            <div class="hexBottom"></div>
+        </div>
+       		
+        <div id="right" class="col-sm-6">
+        	<h2>Witaj! Jestem Iza</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan porttitor viverra. Aliquam consectetur in dui a venenatis. Donec sit amet mauris porttitor, mollis velit eget, ullamcorper ligula. Pellentesque fringilla, metus eget hendrerit fringilla, quam erat efficitur nibh, vel malesuada velit lacus et orci. Donec dictum id ex id molestie. Praesent mollis id lectus ultrices tempus.
+            </p>
         </div>		
-        <div id="right" class="pull-left col-sm-6">
-        	<h2>bla</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan porttitor viverra. Aliquam consectetur in dui a venenatis. Donec sit amet mauris porttitor, mollis velit eget, ullamcorper ligula. Pellentesque fringilla, metus eget hendrerit fringilla, quam erat efficitur nibh, vel malesuada velit lacus et orci. Donec dictum id ex id molestie. Praesent mollis id lectus ultrices tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed fringilla sem ac ante vulputate, facilisis rutrum augue rhoncus. Aliquam porttitor dictum nisl, quis consequat massa convallis at. Nullam nec tellus vitae tortor feugiat aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec vitae posuere ligula. Sed pellentesque interdum urna. Morbi tempor mi sit amet dolor tempor dictum. Curabitur vitae finibus velit, at vehicula ligula. Ut a ex ac risus egestas imperdiet tincidunt id augue.</p>
-        </div>	
-      </div>	
         <div class="end"></div>
 
    
     <?php
 		include 'inc/footer.inc.php';
 	?>
-     </div>
+</div>
     
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script>
